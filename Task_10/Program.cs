e375a08ae42399175b73267bfba759aedc6f307a
Daredevil <aliyor.0276@gmail.com>
2023-08-07 22:01:29 +0500

    first commit

---
+++ Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿int Middle(int x)
+{
+   string result = x.ToString();
+   return int.Parse(result[1].ToString());
+} 
+
+Console.Write("Введите трехзначное число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num.ToString().Length == 3)
+{
+    int middleDigit = Middle(num);
+    Console.Write(middleDigit);
+}
+else
+{
+    Console.WriteLine("Это не трехзначное число!");
+}
