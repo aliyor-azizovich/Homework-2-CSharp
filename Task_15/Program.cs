65594b681dea8b45efab2989e753eb5a0d80fe3b
Daredevil <aliyor.0276@gmail.com>
2023-08-08 20:54:47 +0500

    is day off

---
+++ Task_15/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+bool IsDayOff(int x)
+{
+    if (x ==6 || x==7)
+    {
+        return true;
+    }    
+    
+    else
+    {
+         return false;
+    }
+} 
+
+Console.Write("Enter the number of the day of the week: ");
+
+int num = int.Parse(Console.ReadLine());
+
+bool answer = IsDayOff(num);
+Console.Write(answer ? "Yes" : "No");
+
